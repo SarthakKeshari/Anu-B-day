3d1af3137b2f7cf27f54e7be7c8c2f7e55a0cf01
SarthakKeshari <srthkkeshari84@gmail.com>
2021-01-19 13:23:33 +0530

    bday commit 1

---
+++ pictures.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="gallery_compress_style.css">
+    <title>Chotti B'Day</title>
+  </head>
+  <body>
+        <div class="collapse_"><img src="a (3).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (21).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (22).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (23).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (24).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (25).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (26).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (27).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (28).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (29).jpg" height="725px" alt=""></div>
+        <div class="collapse_"><img src="a (30).jpg" height="725px" alt=""></div>
+        <div height="725px" class="v1">
+            <video src="a (1).mp4" loop autoplay height="400px"></video>
+        </div>
+        <div height="725px">
+            <video src="a (2).mp4" loop autoplay height="400px"></video>
+        </div>
+        <div height="725px" class="v1">
+            <video src="a (3).mp4" loop autoplay height="400px"></video>
+        </div>
+        <div height="725px">
+            <a href="memes.php"><img src="cup.jpeg" alt="" height="400px"></a>
+        </div>
+
+    
+    <!-- Optional JavaScript -->
+    <!-- Popper.js first, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  </body>
+</html>
